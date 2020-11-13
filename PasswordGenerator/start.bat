aea1411d71782cfc6643461c6641d7ae4edc4417
PerkyElixir22 <64582251+PerkyElixir22@users.noreply.github.com>
2020-11-13 11:17:37 +0100

    Update start.bat
    Made it so it tells you if the password generator file is missing

--- PasswordGenerator/start.bat
+++ PasswordGenerator/start.bat
@@ -1,3 +1,5 @@
 @echo off
-if exist bin\bin.lua start "Password Generator" bin\lua\lua.exe bin\bin.lua
-if exist bin\bin.luac start "Password Generator" bin\lua\lua.exe bin\bin.luac
+if exist bin\bin.lua start "Password Generator" bin\lua\lua.exe bin\bin.lua && exit
+if exist bin\bin.luac start "Password Generator" bin\lua\lua.exe bin\bin.luac && exit
+echo Password generator file not found, exiting.
+timeout 5 > nul
